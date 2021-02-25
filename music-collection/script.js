965894d958143c39713684e9fbdd5da248c017cd
martc626 <martc626@newschool.edu>
2021-02-26 06:27:50 +0700

    Music Collection Page

---
+++ music-collection/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	var content = $("content");
+
+	$("#one").click(function() {
+		content.removeClass("orange");
+		content.removeClass("yellow");
+		content.removeClass("green");
+		content.removeClass("blue");
+		content.removeClass("purple");
+	});
+
+	$("#two").click(function() {
+		content.removeClass("red");
+		content.removeClass("yellow");
+		content.removeClass("green");
+		content.removeClass("blue");
+		content.removeClass("purple");
+	});
+
+	$("#three").click(function() {
+		content.removeClass("red");
+		content.removeClass("orange");
+		content.removeClass("green");
+		content.removeClass("blue");
+		content.removeClass("purple");
+	});
+
+	$("#four").click(function() {
+		content.removeClass("red");
+		content.removeClass("orange");
+		content.removeClass("yellow");
+		content.removeClass("blue");
+		content.removeClass("purple");
+	});	
+
+	$("#five").click(function() {
+		content.removeClass("red");
+		content.removeClass("orange");
+		content.removeClass("yellow");
+		content.removeClass("green");
+		content.removeClass("purple");
+	});	
+
+	$("#six").click(function() {
+		content.removeClass("red");
+		content.removeClass("orange");
+		content.removeClass("yellow");
+		content.removeClass("green");
+		content.removeClass("blue");
+	});	
+});
