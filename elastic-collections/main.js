8752948a90b036c4c3793437b028171b664afdd2
martc626 <martc626@newschool.edu>
2021-04-08 20:09:39 +0700

    Elastic Collections Updated

--- elastic-collections/main.js
+++ elastic-collections/main.js
@@ -38,24 +38,81 @@ base('patrons').select({
     var image = document.createElement('img');
     var patronage = document.createElement('div');
 
-    name.classList.add('card__name');
+    name.classList.add('card__name', 'flip_card_front');
     name.innerHTML = record.fields.patron_saints;
 
-    image.classList.add('card__image');
+    image.classList.add('card__image','flip_card_front');
     image.src = record.fields.image[0].url;
 
-    patronage.classList.add('card__patronage');
+    patronage.classList.add('card__patronage','flip_card_front');
     patronage.innerHTML = record.fields.patronage;   
 
     card.classList.add('card');
     card.style.backgroundColor = '#F1F8CA';
 
     // Append the new content to your document
-    card.append(name);
-    card.append(image);
-    card.append(patronage);
+    card.append(name, image, patronage);
     cardContainer.append(card);
+
+    var description = document.createElement('div');
+    var link = document.createElement('div');
+
+    description.classList.add('card__desc','flip_card_back');
+    description.innerHTML = record.fields.description;
+
+    link.classList.add('link','flip_card_back');
+    
+
+
+    var filterAfflictions = document.querySelector('#one');
+    filterAfflictions.addEventListener('click', function() {
+      if (record.fields.category == 'afflictions') {
+        card.style.display = 'inline-block';
+        filterAfflictions.style.textShadow = '0px 0px 8px yellow';
+      } else {
+        card.style.display = 'none';
+        filterEnvironment.style.textShadow = 'none';
+        filterIdentity.style.textShadow = 'none';
+      }
+    });
+
+    var filterEnvironment = document.querySelector('#two');
+    filterEnvironment.addEventListener('click', function() {
+      if (record.fields.category == 'environment') {
+        card.style.display = 'inline-block';
+        filterEnvironment.style.textShadow = '0px 0px 8px yellow';
+      } else {
+        card.style.display = 'none';
+        filterAfflictions.style.textShadow = 'none';
+        filterIdentity.style.textShadow = 'none';
+      }
+    });
+
+    var filterIdentity = document.querySelector('#three');
+    filterIdentity.addEventListener('click', function() {
+      if (record.fields.category == 'identity') {
+        card.style.display = 'inline-block';
+        filterIdentity.style.textShadow = '0px 0px 8px yellow';
+      } else {
+        card.style.display = 'none';
+        filterEnvironment.style.textShadow = 'none';
+        filterAfflictions.style.textShadow = 'none';
+      }
+    });
+
+    var showAll = document.querySelector('#all');
+    showAll.addEventListener('click', function() {
+      if (record.fields.category == 'identity') {
+        card.style.display = 'inline-block';
+        filterIdentity.style.textShadow = '0px 0px 8px yellow';
+      } else {
+        card.style.display = 'none';
+        filterEnvironment.style.textShadow = 'none';
+        filterAfflictions.style.textShadow = 'none';
+      }
+    });
   });
+
 
   // To fetch the next page of records, call `fetchNextPage`.
   // If there are more records, `page` will get called again.
@@ -65,3 +122,8 @@ base('patrons').select({
 }, function done(err) {
   if (err) { console.error(err); return; }
 });
+    function filter(record) {
+    var card = document.querySelector('.card')
+
+ 
+    };
